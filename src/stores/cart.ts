69e56318dadb03f76cc5e42f5d190b3a238bde79
teodoro <teoraulino@gmail.com>
2024-09-15 01:45:23 -0300

    feat: Persist cart

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,5 +1,6 @@
 import { CartItem } from "@/types/Cart";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface CartStore {
   items: CartItem[];
@@ -9,69 +10,83 @@ interface CartStore {
   updateQuantity: (id: string, quantity: number) => void;
   setIsOpen: (isOpen: boolean) => void;
   total: number;
+  clearCart: () => void;
 }
 
-export const useCartStore = create<CartStore>((set) => ({
-  items: [],
-  isOpen: false,
-  total: 0,
-  addItem: (newItem) =>
-    set((state) => {
-      const existingItem = state.items.find((item) => item.id === newItem.id);
-      let updatedItems;
+export const useCartStore = create(
+  persist<CartStore>(
+    (set) => ({
+      items: [],
+      isOpen: false,
+      total: 0,
+      addItem: (newItem) =>
+        set((state) => {
+          const existingItem = state.items.find(
+            (item) => item.id === newItem.id
+          );
+          let updatedItems;
 
-      if (existingItem) {
-        updatedItems = state.items.map((item) =>
-          item.id === newItem.id
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        );
-      } else {
-        updatedItems = [...state.items, { ...newItem, quantity: 1 }];
-      }
+          if (existingItem) {
+            updatedItems = state.items.map((item) =>
+              item.id === newItem.id
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+            );
+          } else {
+            updatedItems = [...state.items, { ...newItem, quantity: 1 }];
+          }
 
-      const newTotal = updatedItems.reduce(
-        (sum, item) => sum + item.price * item.quantity,
-        0
-      );
+          const newTotal = updatedItems.reduce(
+            (sum, item) => sum + item.price * item.quantity,
+            0
+          );
 
-      return {
-        items: updatedItems,
-        isOpen: true,
-        total: newTotal,
-      };
+          return {
+            items: updatedItems,
+            isOpen: true,
+            total: newTotal,
+          };
+        }),
+      removeItem: (id) =>
+        set((state) => {
+          const updatedItems = state.items.filter((item) => item.id !== id);
+
+          const newTotal = updatedItems.reduce(
+            (sum, item) => sum + item.price * item.quantity,
+            0
+          );
+
+          return {
+            items: updatedItems,
+            total: newTotal,
+          };
+        }),
+      updateQuantity: (id, quantity) =>
+        set((state) => {
+          const updatedItems = state.items
+            .map((item) =>
+              item.id === id
+                ? { ...item, quantity: Math.max(0, quantity) }
+                : item
+            )
+            .filter((item) => item.quantity > 0);
+
+          const newTotal = updatedItems.reduce(
+            (sum, item) => sum + item.price * item.quantity,
+            0
+          );
+
+          return {
+            items: updatedItems,
+            total: newTotal,
+          };
+        }),
+      setIsOpen: (isOpen) => set({ isOpen }),
+      clearCart: () => set({ items: [], total: 0 }),
     }),
-  removeItem: (id) =>
-    set((state) => {
-      const updatedItems = state.items.filter((item) => item.id !== id);
-
-      const newTotal = updatedItems.reduce(
-        (sum, item) => sum + item.price * item.quantity,
-        0
-      );
-
-      return {
-        items: updatedItems,
-        total: newTotal,
-      };
-    }),
-  updateQuantity: (id, quantity) =>
-    set((state) => {
-      const updatedItems = state.items
-        .map((item) =>
-          item.id === id ? { ...item, quantity: Math.max(0, quantity) } : item
-        )
-        .filter((item) => item.quantity > 0);
-
-      const newTotal = updatedItems.reduce(
-        (sum, item) => sum + item.price * item.quantity,
-        0
-      );
-
-      return {
-        items: updatedItems,
-        total: newTotal,
-      };
-    }),
-  setIsOpen: (isOpen) => set({ isOpen }),
-}));
+    {
+      name: "cart",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
